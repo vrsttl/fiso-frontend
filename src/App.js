26bc30d30523efdff3fb0bc3aacd1cf3a7494428
Attila <vrsttl@gmail.com>
2021-08-24 18:04:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,202 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { cloneDeep } from 'lodash';
+import axios from "axios";
+import * as CONSTANTS from './constants';
 import './App.css';
 
 function App() {
+  const [stakeKey, setStakeKey] = useState(CONSTANTS.stakeKey);
+  const [errors, setErrors] = useState({});
+  const [liveDelegation, setLiveDelegation] = useState(null);
+  const [currentPoolTicker, setCurrentPoolTicker] = useState(null);
+  const [allDelegators, setAllDelegators] = useState(null);
+  const [extendedDelegatorObject, setExtendedDelegatorObject] = useState(null);
+  const [delegatorWithTimestamp, setDelegatorWithTimestamp] = useState(null);
+  const [slots, setSlots] = useState([]);
+ 
+  useEffect(() => {
+    if (liveDelegation) {
+      setCurrentPoolTicker(
+        CONSTANTS.allPools.find(
+          pool => pool.value === liveDelegation.pool_id
+        ).label
+      )
+      CONSTANTS.originalPools.forEach(async ({ value, label }) =>
+        await getDelegatorsForPool(value, label));
+    }
+  }, [liveDelegation]);
+
+  useEffect(() => {
+    if (
+      allDelegators &&
+      Object.keys(allDelegators).length &&
+      Object.keys(allDelegators).length === 10
+    ) {
+      let delegatorWorkingObject = allDelegators;
+      const getTxHash = async (ticker) => {
+        const delegators = allDelegators[ticker];
+        const stakeKeys = await delegators.map(delegator => delegator.address);
+        if (stakeKeys) {
+          const txs = stakeKeys.map(async key => await axios.get(
+            `${CONSTANTS.blockfrostAPI}accounts/${key}/delegations/?order=desc`,
+            CONSTANTS.config
+          ));
+          if (txs) {
+            Promise
+              .all(txs)
+              .then(items => {
+                items.forEach(
+                  (item, index) => {
+                  delegatorWorkingObject[ticker][index] = {
+                    ...delegators[index],
+                    tx: item.data[0].tx_hash,
+                  };
+              })})
+              .finally(setExtendedDelegatorObject(delegatorWorkingObject));
+        }
+      }
+    }
+      getTxHash("ADAHS");
+    }
+  }, [allDelegators]);
+
+  const handleSetKey = key => {
+    if (key || key === "") {
+      setStakeKey(key);
+    };
+  };
+
+  const getStakingActivity = async () => {
+    try {
+      const delegations = await axios.get(
+        `${CONSTANTS.blockfrostAPI}accounts/${stakeKey}/delegations/?order=desc`,
+        CONSTANTS.config
+      );
+      if (delegations && !delegations.hasOwnProperty("error")) {
+        setLiveDelegation(delegations.data[0]);
+        setErrors({});
+      } 
+    }
+    catch (error) {
+      setErrors({
+        error: error.response.data.message,
+      });
+    };
+  };
+
+  useEffect(() => {
+    if (extendedDelegatorObject) {
+      console.log("extended", extendedDelegatorObject);
+      const delegatorWorkingObject = { ...extendedDelegatorObject };
+      setTimeout(() => {
+        const slots = extendedDelegatorObject['ADAHS'].map(
+          async delegator => {
+            if (delegator.tx) {
+              return await axios.get(
+              `${CONSTANTS.blockfrostAPI}txs/${delegator.tx}`,
+              CONSTANTS.config
+            )}
+          })
+        if (slots) {
+          const newItem = {};
+          let newSlots = [];
+          for (let key in delegatorWorkingObject) {
+            newItem[key] = delegatorWorkingObject[key]
+          };
+          Promise
+            .all(slots)
+            .then(items => {
+              items.forEach(
+                (item, index) => {
+                  newSlots = [...newSlots, item.data.slot];
+                  newItem['ADAHS'][index] = {
+                  ...delegatorWorkingObject['ADAHS'][index],
+                  slot: item.data.slot,
+                };
+            })})
+            .finally(() => {
+              setSlots(newSlots);
+              setDelegatorWithTimestamp(newItem)
+            });
+        }
+      }, 40000)
+    }
+  }, [extendedDelegatorObject]);
+
+  useEffect(() => {
+    if (delegatorWithTimestamp) {
+      console.log("slots", slots);
+      console.log("withTimestamp", delegatorWithTimestamp);
+      const updatedList = delegatorWithTimestamp['ADAHS'].map((el, index) => {
+        return {
+          ...el,
+          slot: slots[index],
+        }
+      });
+      const intersectionTime = 37041094;
+      console.log("intersectiontime", intersectionTime);
+      const filteredDelegations =
+        updatedList.filter(
+            el => el.slot < intersectionTime
+          );
+      console.log("newItem", updatedList);
+      console.log("filtered", filteredDelegations);
+      const sumLiveStake =
+        filteredDelegations.reduce(
+          (acc, delegation) => acc + +delegation.live_stake, 0
+      );
+      console.log("sumLiveStake", sumLiveStake);
+    }
+  }, [delegatorWithTimestamp, stakeKey, slots]);
+
+  const getDelegatorsForPool = async (poolId, ticker) => {
+    try {
+      const getDelegators = async (number, list = []) => {
+        let partialDelegators = cloneDeep(list);
+        const newCall =  await axios.get(
+          `${CONSTANTS.blockfrostAPI}pools/${poolId}/delegators/?page=${number}`,
+          CONSTANTS.config
+        );
+        if (newCall.data) {
+          partialDelegators = [...partialDelegators, ...newCall.data];
+        }
+        if (partialDelegators.length % 100 === 0 && newCall.data && newCall.data.length > 0) {
+          number++;
+          return await getDelegators(number, partialDelegators);
+        } else if (partialDelegators) {
+          return partialDelegators;
+        }
+      }
+      const delegators = await getDelegators(1);
+      if (delegators && !delegators.hasOwnProperty("error")) {
+        setAllDelegators(prev => ({ ...prev, [ticker]: delegators }));
+        setErrors({});
+      } 
+    }
+    catch (error) {
+      setErrors({
+        error: error.response,
+      });
+    };
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Input stake key here</h2>
+      <input
+        type="text"
+        onChange={({ target }) => handleSetKey(target.value)}
+        value={stakeKey}
+      />
+      <button onClick={getStakingActivity} disabled={!stakeKey}>Check your stake pool status</button>
+      {currentPoolTicker && (
+        <>
+          <div>Your current pool is:</div>
+          <div>{currentPoolTicker}</div>
+        </>
+        )
+      }
+      {errors.error && <div>{errors.error}</div>}
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,115 @@
+export const originalPools = [
+  {
+    label: "SWELL",
+    value: "pool10let2ne3rdnhf6pvhauqcsf7f83hkagp4lfs792frrmu5up7xa7"
+  },
+  {
+    label: "MALU",
+    value: "pool18zf8txwv8lmtpq2src8wrhz0pjut5qft8h5tfxnctwc95r7jvvj"
+  },
+  {
+    label: "DEGAS",
+    value: "pool1g9v6nnrn8x7jcj26mqernsh3h97g6gyhamttzg8q39yvjr60g7k"
+  },
+  {
+    label: "BLADE",
+    value: "pool16ajaae2n5lsyr4f9k9uz5y8tpf0996tw640dzu7chwp2wdrnz6a"
+  },
+  {
+    label: "AMOON",
+    value: "pool1qkd5y9azf2xx0gmrj68lrkcn59ldjmnpzd3y2rq3tvjp2vqljan"
+  },
+  {
+    label: "BACK",
+    value: "pool1xmn8ag7jctcpdqk9jtrzk9s7qgs4f3cehkxhtelgpag6c7fjxdm"
+  },
+  {
+    label: "GINGR",
+    value: "pool1rhq7qyw4y2s80gstyevm6rhz2lsdsrcstawunxx5eld8csvuvlu"
+  },
+  {
+    label: "VAULT",
+    value: "pool1k53pf4wzn263c08e3wr3gttndfecm9f4uzekgctcx947vt7fh2p"
+  },
+  {
+    label: "RSV",
+    value: "pool1r0sxz0hygfc0266lhdclve6j3cwcwlth6dya6uey65yxjnwm7qc"
+  },
+  {
+    label: "ADAHS",
+    value: "pool12gqhv4vf0y7vngzd3umchjz3yhkqaaalwzltsngpdykgyud5rhz"
+  },
+];
+
+export const extendedPools = [
+  {
+    label: "NOVA",
+    value: "pool1sf784g6eje8vzq9f5wu583eurhfq2ee9hzyhl46yezkuqqtmw07"
+  },
+  {
+    label: "JUST",
+    value: "pool1dcjycc5mvj9svh9ys482078mnqutu48k3zj7uqzswj7m54a7hzr"
+  },
+  {
+    label: "SOLIA",
+    value: "pool1a6ccp9fpgn40r2jl7fapjv3224swqf72z5gt4cgwctnk5etfjlr"
+  },
+  {
+    label: "COINZ",
+    value: "pool1jxv3my3c70e8pfr0cgm6esatehey2ml3v0qk8x236aw32eu0fcy"
+  },
+  {
+    label: "EU01",
+    value: "pool1wm5quxelvgk8q50jyfznf9asvelp9zf0tk2wu4jazlw9yj4uctw"
+  },
+  {
+    label: "HYPE",
+    value: "pool1980svf2cxnfy66s5jgjv7y22xud75z9kfe6n0c3y54s62w56435"
+  },
+  {
+    label: "ADATA",
+    value: "pool1adhpjj009c0jekumqswj5jux6nvrw7v2k0wlu57vfjh7gd5wyh9"
+  },
+  {
+    label: "ALFA",
+    value: "pool18qddv82sx8p22uxzdrf0qx9lkewxadlr9ef45kpcf7dzz3n56qv"
+  },
+  {
+    label: "ACI",
+    value: "pool144m894gswuy3se407ma5870nkaw5ylykrak73gf4kpepj50ulfa"
+  },
+  {
+    label: "EUSKL",
+    value: "pool1ksfzungz56vxa9sy6hr54v5hf6qx8rmcu6734gqm3qlxucveed6"
+  },
+  {
+    label: "QCPOL",
+    value: "pool1c2utlagkpht4zj0jetsf245c258geuxnjqp9kf4f2z9rutx9dz4"
+  },
+  {
+    label: "ALPCH",
+    value: "pool18r2y72aue5nmv489xtnfxl36vzusq95qst6urd87yd5hgzms04c"
+  },
+  {
+    label: "YOADA",
+    value: "pool1je2n5smmnyn3lh92xrp8nepjyfp596ypga6pmdecwtcrcj9uvec"
+  },
+  {
+    label: "TREE",
+    value: "pool1ktgug7wwf9k4hcsuk68ftkh8xs5n0333fzxyw9sagz8njhlskgm"
+  },
+  {
+    label: "FUNGI",
+    value: "pool1nh9j5qucsddxkzn3m4dm4gxkhwtpya4wm4e0ye2u0puaqgkcm65"
+  },
+];
+
+export const allPools = [...originalPools, ...extendedPools];
+
+export const blockfrostAPI = "https://cardano-mainnet.blockfrost.io/api/v0/";
+
+export const stakeKey = "stake1ux6vf8h48yccmhfcqvyzy3t7c0d9sanz4njc2dyhgctv7sseyvy5p";
+
+export const config = {
+  headers: { 'project_id': 'EkAw4OsTQ7QY8I2G4jRXbnBMdiI07Qae'},
+};
